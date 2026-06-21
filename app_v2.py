aa3f6b97882ab3f3a7ce9a857d7e4359bd2820ff
aryajaitapkar21-ship-it <aryajaitapkar21@gmail.com>
2026-06-21 14:03:26 +0000

    Increased Pagee title  font size

--- app_v2.py
+++ app_v2.py
@@ -56,7 +56,7 @@ html, body, [class*="css"] {
 }
 
 h1 {
-  font-size: 2.5rem !important;
+  font-size: 3.5rem !important;
   font-weight: 800 !important;
   letter-spacing: -0.03em !important;
   line-height: 1.2 !important;
